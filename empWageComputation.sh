bde82c1bc7e65da0894ab37b284381af08dc300c
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-25 12:35:00 +0530

    REFACTORED : Constant variable IS_PRESENT is Added.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,12 +4,15 @@ printf "Welcome To Employee Wage Computation\n"
 
 #CONSTANTS
 RANDOM_END=2
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+IS_PRESENT=1
 
 #VARIABLES
 randomNumber=0
 randomNumber=$(( RANDOM % RANDOM_END ))
 
-if [ $randomNumber -eq 1 ]
+if [ $randomNumber -eq $IS_PRESENT ]
 	then
 		printf "Present\n"
 	else
