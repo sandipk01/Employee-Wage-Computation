3ca42ec5dd8ba1c2313a3dc127902be98c73230e
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-25 15:45:34 +0530

    REFACTORED : Function for get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -23,27 +23,38 @@ totalSalary=0
 totalWorkingHours=0
 days=0
 employeeHours=0
+hours=0
 
+#GET HOURS 8 or 4
+function getHours(){
+	 case $1 in
+		$IS_FULL_DAY)
+			echo $FULL_DAY_HOUR
+		;;
+		$IS_HALF_DAY)
+			echo $HALF_DAY_HOUR
+		;;
+		*)
+			echo 0
+		;;
+	esac
+}
+
+#WHILE DAY IS LESS THAN 20 AND HOURS IS LESS THAN 100 TILL CHECK
 while [ $days -lt $MONTH -a $totalWorkingHours -lt $TOTAL_HOURS ]
 do
 	days=$(( $days + 1 ))
 	case $randomNumber in
 		$IS_PRESENT)
 			attendance="Present"
-			case $randomNumber2 in
-				$IS_FULL_DAY)
-					salary=$(( $WAGE_PER_HOUR * $FULL_DAY_HOUR ))
-					employeeHours=$FULL_DAY_HOUR
-			;;
-				$IS_HALF_DAY)
-					salary=$(( $WAGE_PER_HOUR * $HALF_DAY_HOUR ))
-					employeeHours=$HALF_DAY_HOUR
-			;;
-			esac
+			hours="$( getHours $(( $randomNumber2 )) )"
+			salary=$(( $WAGE_PER_HOUR * $hours ))
+			employeeHours=$hours
 		;;
-		*)
+		0)
 			attendance="Absent"
 			employeeHours=0
+		;;
 	esac
    	totalSalary=$(( $totalSalary + $salary ))
 		totalWorkingHours=$(( $totalWorkingHours + $employeeHours ))
