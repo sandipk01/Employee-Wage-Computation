5c5cd59f58654facebf3ba2233ebded329084c0d
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-25 12:22:03 +0530

    REFACTORED : Checking employee Present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
-printf "Welcome To Employee Wage Computation"
+printf "Welcome To Employee Wage Computation\n"
+
+#CONSTANTS
+RANDOM_END=2
+
+#VARIABLES
+randomNumber=0
+randomNumber=$(( RANDOM % RANDOM_END ))
+
+if [ $randomNumber -eq 1 ]
+	then
+		printf "Present\n"
+	else
+		printf "Absent\n"
+fi
