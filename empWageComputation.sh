2593f02afc04567f2d0bdbc560751ead3ba650db
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-25 15:06:46 +0530

    REFACTORED : Calculated Wages till condition working hours 100 and days 20

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,6 +10,8 @@ HALF_DAY_HOUR=4
 IS_PRESENT=1
 IS_FULL_DAY=0
 IS_HALF_DAY=1
+MONTH=20
+TOTAL_HOURS=100
 
 #VARIABLES
 randomNumber=0
@@ -18,25 +20,32 @@ randomNumber2=$(( RANDOM % $RANDOM_END ))
 salary=0
 attendance=0
 totalSalary=0
-months=20
+totalWorkingHours=0
+days=0
+employeeHours=0
 
-for(( index=0; index<20; index++ ))
+while [ $days -lt $MONTH -a $totalWorkingHours -lt $TOTAL_HOURS ]
 do
+	days=$(( $days + 1 ))
 	case $randomNumber in
 		$IS_PRESENT)
 			attendance="Present"
 			case $randomNumber2 in
 				$IS_FULL_DAY)
 					salary=$(( $WAGE_PER_HOUR * $FULL_DAY_HOUR ))
+					employeeHours=$FULL_DAY_HOUR
 			;;
 				$IS_HALF_DAY)
 					salary=$(( $WAGE_PER_HOUR * $HALF_DAY_HOUR ))
+					employeeHours=$HALF_DAY_HOUR
 			;;
 			esac
 		;;
 		*)
 			attendance="Absent"
+			employeeHours=0
 	esac
    	totalSalary=$(( $totalSalary + $salary ))
+		totalWorkingHours=$(( $totalWorkingHours + $employeeHours ))
 done
 	printf " Employee monthly salary is : $totalSalary \n"
